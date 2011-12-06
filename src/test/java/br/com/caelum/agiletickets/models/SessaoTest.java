cd68e46f924c4acf93b28ff6686bdb6b333c6f0b
Fabricio Nogueira <fabricio.nogueira@gmail.com>
2011-12-06 11:32:21 -0200

    Correção de bug no método podeReservar()

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -30,4 +30,15 @@ public class SessaoTest {
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
+	
+	@Test
+	public void deveVenderTodosIngressosDisponiveis() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(2);
+		
+		Assert.assertTrue(sessao.podeReservar(2));
+	}
+	
+	
+	
 }
