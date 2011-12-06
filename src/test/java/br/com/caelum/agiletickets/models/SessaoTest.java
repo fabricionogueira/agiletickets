9776aff0c4a179b4593fdcd95e79e5b9e67e8514
Fabricio Nogueira <fabricio.nogueira@gmail.com>
2011-12-06 12:00:28 -0200

    Refatoração do teste.

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,14 +1,21 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
 
+	Sessao sessao;
 
+	@Before
+	public void inicializa() {
+		sessao = new Sessao();
+	}
+	
 	@Test
 	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
+		
         sessao.setTotalIngressos(2);
 
         Assert.assertTrue(sessao.podeReservar(1));
@@ -16,7 +23,7 @@ public class SessaoTest {
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
+		
 		sessao.setTotalIngressos(2);
 
 		Assert.assertFalse(sessao.podeReservar(3));
@@ -24,7 +31,7 @@ public class SessaoTest {
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
+		
 		sessao.setTotalIngressos(5);
 
 		sessao.reserva(3);
@@ -33,7 +40,6 @@ public class SessaoTest {
 	
 	@Test
 	public void deveVenderTodosIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
 		
 		Assert.assertTrue(sessao.podeReservar(2));
